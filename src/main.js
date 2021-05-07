cc8a96aca735874350af2a6366b1b223fd5c87f2
Rollors <18270387673@163.com>
2021-05-07 16:01:52 +0800

    项目结构修改

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './styles/index.less'
 Vue.config.productionTip = false
 
+// 创建Vue根实例，将router,store配置到根实例中
+// 把App根组件渲染到#app节点
 new Vue({
   router,
   store,
