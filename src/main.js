b652b9f10f5de5d30c64921703142a0a9a26aceb
Rollors <18270387673@163.com>
2021-05-08 10:10:09 +0800

    项目初始化完成及登录页面结构搭建

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+
+// 加载Vant组件库,样式不适配所有设备，需要解决适配问题
+import Vant from 'vant'
+
+// 加载Vant组件库样式
+import 'vant/lib/index.css'
+
+// 自动设置rem基准值(html标签的字体大小)
+import 'amfe-flexible'
+
+// 全局注册Vant中的组件
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 // 创建Vue根实例，将router,store配置到根实例中
